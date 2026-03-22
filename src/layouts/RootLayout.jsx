f6ded98612d5a62dfefa9ca3d0a66ac80f8ce9bc
Peter Mensah <ing.mensah@outlook.com>
2026-03-22 15:16:19 +0000

    widget integrated

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
+import BoafoWidgetInitializer from "../components/BoafoWidgetInitializer";
 
 const RootLayout = () => {
   return (
     <div>
+      <BoafoWidgetInitializer />
       <Navbar />
       <Outlet />
       <Footer />
